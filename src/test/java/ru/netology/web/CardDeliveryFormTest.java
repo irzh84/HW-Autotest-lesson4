4faee525597d1dbba7f3f91c8d859287a964bb39
Irina <izhts1482@gmail.com>
2023-11-18 14:15:29 +0300

    Positive test

--- src/test/java/ru/netology/web/CardDeliveryFormTest.java
+++ src/test/java/ru/netology/web/CardDeliveryFormTest.java
@@ -1,12 +1,25 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryFormTest {
     @Test
     void shouldPositiveTest() {
         open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Казань");
+        $("[data-test-id='date'] input").setValue("25.11.2023");
+        $("[data-test-id='name'] input").setValue("Иван Петров");
+        $("[data-test-id='phone'] input").setValue("+79010010101");
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        // $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
     }
 }
