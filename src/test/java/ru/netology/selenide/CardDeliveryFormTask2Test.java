4c3839a812e186d669df8f71b62b994ec3440d4d
Irina <izhts1482@gmail.com>
2023-11-19 20:46:29 +0300

    Task 2 edited

--- src/test/java/ru/netology/selenide/CardDeliveryFormTask2Test.java
+++ src/test/java/ru/netology/selenide/CardDeliveryFormTask2Test.java
@@ -18,12 +18,15 @@ public class CardDeliveryFormTask2Test {
     }
 
     @Test
-    void shouldChooseTownDropDownList() {
+    void shouldChooseTownAndDay() {
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Мо");
         $$(".menu-item__control").findBy(text("Москва")).click();
-        String planningDate = generateDate(3, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        String planningDate = generateDate(5, "dd.MM.yyyy");
+        if (!generateDate(5, "MM").equals(generateDate(7, "MM"))) {
+            $("[data-step='1']").click();
+        };
         $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Иван Петров");
         $("[data-test-id='phone'] input").setValue("+79010010101");
@@ -32,42 +35,6 @@ public class CardDeliveryFormTask2Test {
         $(".notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
-    }
-
-    @Test
-    void shouldChooseDateCalendarForm() {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Казань");
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        if (!generateDate(3, "MM").equals(generateDate(7, "MM"))) {
-            $("[data-step='1']").click();
-        };
-        $$("[data-day]").findBy(text("24")).click();
-        $("[data-test-id='name'] input").setValue("Иван Петров");
-        $("[data-test-id='phone'] input").setValue("+79010010101");
-        $("[data-test-id='agreement']").click();
-        $(".button").click();
-        $(".notification__content")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на 24.11.2023"));
-    }
-
-    @Test
-    void shouldChooseDateCalendarFormDecember() {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Казань");
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        if (!generateDate(7, "MM").equals(generateDate(14, "MM"))) {
-            $("[data-step='1']").click();
-        };
-        $$("[data-day]").findBy(text("2")).click();
-        $("[data-test-id='name'] input").setValue("Иван Петров");
-        $("[data-test-id='phone'] input").setValue("+79010010101");
-        $("[data-test-id='agreement']").click();
-        $(".button").click();
-        $(".notification__content")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на 02.12.2023"));
     }
 }
 
